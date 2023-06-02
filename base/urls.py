779d26d5e2dc09c4a1acc2719c7bdf1df83f6536
kamworor <51693048+kamworor@users.noreply.github.com>
2023-06-02 18:43:20 +0300

    sample proj

--- base/urls.py
+++ base/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('login/', views.loginUser, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+    path('register/', views.registerUser, name="register"),
+
     # Add more URL patterns here
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+def loginUser(request):
+    page = 'login'
+
+    context = {'page': page}
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, "User does not exist.")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+             messages.error(request, "Username OR password does not exist!! .")
+
+    return render(request, 'base/login_register.html', context)    
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
+
+def registerUser(request): 
+    form = UserCreationForm()
+    return render(request, 'base/login_register.html', {'form':form})
+
 def home(request):
-    return render(request,'home.html' )
+    return render(request,'base/home.html' )
+
+
+
+
